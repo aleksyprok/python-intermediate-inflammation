2659df5c1030f64528415a6ea9f96b06a4d9375b
Alex Prokopyszyn <aleksyprok@gmail.com>
2023-07-18 13:25:44 +0100

    Updates

--- tests/test_models.py
+++ tests/test_models.py
@@ -63,3 +63,29 @@ def test_daily_min_string():
 
     with pytest.raises(TypeError):
         daily_min([['Hello', 'there'], ['General', 'Kenobi']])
+
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+         [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]])
+    ])
+def test_patient_normalise(test, expected):
+    """Test normalisation works for arrays of one and positive integers.
+       Assumption that test accuracy of two decimal places is sufficient."""
+    from inflammation.models import patient_normalise
+    npt.assert_almost_equal(patient_normalise(np.array(test)),
+                            np.array(expected),
+                            decimal=2)
+
+
+def test_random_numpy():
+    mean = 5
+    sdev = 3
+    sample_size = 1000000
+
+    sample = np.random.normal(mean, sdev, sample_size)
+
+    np.testing.assert_almost_equal(mean, np.mean(sample), decimal=2)
+    np.testing.assert_almost_equal(sdev, np.std(sample), decimal=2)
